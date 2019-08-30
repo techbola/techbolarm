d89f10aab85ae74322a08208592fbbb2fcf5292b
techbola <bfagbowore@gmail.com>
2019-08-30 02:14:45 +0100

    menu vue components

---
+++ resources/js/modules/menu/MenuContainer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="wrapper menu__container">
+        <p>This is the menu container</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'items'
+        ],
+        name: "MenuContainer"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,8 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <menu-container :items="{{ json_encode($menus) }}"></menu-container>
+
                 </div>
             </div>
         </div>
